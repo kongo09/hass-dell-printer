2de8636921b04218286894b51c3d9db2c9100713
kongo09 <kongo09@gmail.com>
2022-01-01 20:02:30 +0100

    prepare data update coordinator

--- custom_components/dell_printer/__init__.py
+++ custom_components/dell_printer/__init__.py
@@ -1,17 +1,18 @@
 """The Dell printer component."""
 from __future__ import annotations
+from typing import Dict
 
 from dell_printer_parser.printer_parser import DellPrinterParser
 
-from .const import DOMAIN, PLATFORMS
+from .const import *
 
 from homeassistant.core import HomeAssistant
-from homeassistant.const import CONF_HOST
+from homeassistant.const import CONF_HOST, CONF_SCAN_INTERVAL
 from homeassistant.config_entries import ConfigEntry
 
 from homeassistant.helpers.update_coordinator import DataUpdateCoordinator, UpdateFailed
 
-
+import async_timeout
 import logging
 
 _LOGGER = logging.getLogger(__name__)
@@ -20,57 +21,98 @@ _LOGGER = logging.getLogger(__name__)
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Setup a Dell printer from a config entry."""
 
-     _LOGGER.debug(f"async_setup_entry: {entry}")
+    _LOGGER.debug(f"async_setup_entry: {entry}")
 
     # get the host address
     host = entry.data[CONF_HOST]
 
     # setup the parser
+    update_interval = entry.data[CONF_SCAN_INTERVAL]
     session = async_get_clientsession(hass)
     printer = DellPrinterParser(session, host)
 
-    # get a coordinator
-    coordinator = DellDataUpdateCoordinator(hass, printer)
+    # setup a coordinator
+    coordinator = DellDataUpdateCoordinator(hass, printer, update_interval)
     await coordinator.async_config_entry_first_refresh()
+    
+    # store coordinator
+    hass.data.setdefault(DOMAIN, {})
+    hass.data[DOMAIN][DATA_CONFIG_ENTRY][entry.entry_id] = coordinator
+
+    # setup sensors
+    hass.config_entries.async_setup_platforms(entry, PLATFORMS)
 
     return True
 
 
-async def async_unload_entry(hass: HomeAssistant, entry):
-    await hass.data[DOMAIN][entry.entry_id].unload()
-    for p in PLATFORMS:
-        await hass.config_entries.async_forward_entry_unload(entry, p)
-    hass.data[DOMAIN].pop(entry.entry_id)
-    return True
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
+    """Unload a config entry."""
+    
+    unload_ok = await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
 
+    if unload_ok:
+        hass.data[DOMAIN][DATA_CONFIG_ENTRY].pop(entry.entry_id)
+        if not hass.data[DOMAIN][DATA_CONFIG_ENTRY]:
+            hass.data[DOMAIN].pop(DATA_CONFIG_ENTRY)
 
-async def async_setup(hass: HomeAssistant, config) -> bool:
-    hass.data[DOMAIN] = dict()
-    return True
+    return unload_ok
 
 
 class DellDataUpdateCoordinator(DataUpdateCoordinator):
-    """Class to manage fetching data from Dell printer."""
+    """Class to manage fetching Dell data from the printer."""
 
-    def __init__(
-        self, hass: HomeAssistant, printer: DellPrinterParser
-    ) -> None:
+    def __init__(self, hass: HomeAssistant, printer: DellPrinterParser, update_interval: int) -> None:
         """Initialize."""
 
-        self.printer = DellPrinterParser(session, host)
+        super().__init__(hass, _LOGGER, name=DOMAIN, update_interval=update_interval)
+        self.printer = printer
 
-        super().__init__(
-            hass,
-            _LOGGER,
-            name=DOMAIN,
-            update_interval=SCAN_INTERVAL
-        )
 
-    async def _async_update_data(self) -> DictToObj:
+    async def _async_update_data(self) -> Dict:
         """Update data via library."""
 
         try:
-            data = await self.brother.async_update()
+            """Ask the library to reload fresh data."""
+            self.printer.load_data()
         except (ConnectionError) as error:
             raise UpdateFailed(error) from error
+
+        """Stick the data into a dictionary and return this for further usage."""
+        data = {}
+
+        data[MODEL_NAME] = self.printer.information.modelName
+        data[DELL_SERVICE_TAG_NUMBER] = self.printer.information.dellServiceTagNumber
+        data[ASSET_TAG_NUMBER] = self.printer.information.assetTagNumber
+        data[PRINTER_SERIAL_NUMBER] = self.printer.information.printerSerialNumber
+        data[MEMORY_CAPACITY] = self.printer.information.memoryCapacity
+        data[PROCESSOR_SPEED] = self.printer.information.processorSpeed
+        data[FIRMWARE_VERSION] = self.printer.information.firmwareVersion
+        data[NETWORK_FIRMWARE_VERSION] = self.printer.information.networkFirmwareVersion
+        data[CYAN_LEVEL] = self.printer.status.cyanLevel
+        data[MAGENTA_LEVEL] = self.printer.status.magentaLevel
+        data[YELLOW_LEVEL] = self.printer.status.yellowLevel
+        data[BLACK_LEVEL] = self.printer.status.blackLevel
+        data[MULTI_PURPOSE_FEEDER_STATUS] = self.printer.status.multiPurposeFeederStatus
+        data[MULTI_PURPOSE_FEEDER_CAPACITY] = self.printer.status.multiPurposeFeederCapacity
+        data[MULTI_PURPOSE_FEEDER_SIZE] = self.printer.status.multiPurposeFeederSize
+        data[OUTPUT_TRAY_STATUS] = self.printer.status.outputTrayStatus
+        data[OUTPUT_TRAY_CAPACITY] = self.printer.status.outputTrayCapacity
+        data[REAR_COVER_STATUS] = self.printer.status.rearCoverStatus
+        data[ADF_COVER_STATUS] = self.printer.status.adfCoverStatus
+        data[PRINTER_TYPE] = self.printer.status.printerType
+        data[PRINTING_SPEED] = self.printer.status.printingSpeed
+        data[PRINTER_PAGE_COUNT] = self.printer.printVolume.printerPageCount
+        data[PAPER_USED_LETTER] = self.printer.printVolume.paperUsedLetter
+        data[PAPER_USED_A5] = self.printer.printVolume.paperUsedA5
+        data[PAPER_USED_B5] = self.printer.printVolume.paperUsedB5
+        data[PAPER_USED_A4] = self.printer.printVolume.paperUsedA4
+        data[PAPER_USED_EXECUTIVE] = self.printer.printVolume.paperUsedExecutive
+        data[PAPER_USED_FOLIO] = self.printer.printVolume.paperUsedFolio
+        data[PAPER_USED_LEGAL] = self.printer.printVolume.paperUsedLegal
+        data[PAPER_USED_ENVELOPE] = self.printer.printVolume.paperUsedEnvelope
+        data[PAPER_USED_MONARCH] = self.printer.printVolume.paperUsedMonarch
+        data[PAPER_USED_DL] = self.printer.printVolume.paperUsedDL
+        data[PAPER_USED_C5] = self.printer.printVolume.paperUsedC5
+        data[PAPER_USED_OTHERS] = self.printer.printVolume.paperUsedOthers
+
         return data
--- custom_components/dell_printer/const.py
+++ custom_components/dell_printer/const.py
@@ -1,8 +1,55 @@
 DOMAIN = "dell_printer"
 PLATFORMS = ["sensor"]
+DATA_CONFIG_ENTRY = "config_entry"
 
 # set polling interval to 30s
 POLLING_INTERVAL = 30
 
 # configuration parameters
 DEFAULT_NAME = "Dell Printer"
+
+# data keys
+PRINTER_INFORMATION = "printer_information"
+PRINTER_STATUS = "printer_status"
+PRINTER_PRINT_VOLUME = "printer_print_volume"
+
+MODEL_NAME = "model_name"
+DELL_SERVICE_TAG_NUMBER = "dell_service_tag_number"
+ASSET_TAG_NUMBER = "asset_tag_number"
+PRINTER_SERIAL_NUMBER = "printer_serial_number"
+MEMORY_CAPACITY = "memory_capacity"
+PROCESSOR_SPEED = "processor_speed"
+FIRMWARE_VERSION = "firmware_version"
+NETWORK_FIRMWARE_VERSION = "network_firmware_version"
+
+CYAN_LEVEL = "cyan_level"
+MAGENTA_LEVEL = "magental_level"
+YELLOW_LEVEL = "yellow_level"
+BLACK_LEVEL = "black_level"
+
+MULTI_PURPOSE_FEEDER_STATUS = "multi_purpose_feeder_status"
+MULTI_PURPOSE_FEEDER_CAPACITY = "multi_purpose_feeder_capacity"
+MULTI_PURPOSE_FEEDER_SIZE = "multi_purpose_feeder_size"
+
+OUTPUT_TRAY_STATUS = "output_tray_status"
+OUTPUT_TRAY_CAPACITY = "output_tray_capacity"
+
+REAR_COVER_STATUS = "rear_cover_status"
+ADF_COVER_STATUS = "adf_cover_status"
+
+PRINTER_TYPE = "printer_type"
+PRINTING_SPEED = "printing_speed"
+
+PRINTER_PAGE_COUNT = "printer_page_count"
+PAPER_USED_LETTER = "paper_used_letter"
+PAPER_USED_B5 = "paper_used_b5"
+PAPER_USED_A5 = "paper_used_a5"
+PAPER_USED_A4 = "paper_used_a4"
+PAPER_USED_EXECUTIVE = "paper_used_executive"
+PAPER_USED_FOLIO = "paper_used_folio"
+PAPER_USED_LEGAL = "paper_used_legal"
+PAPER_USED_ENVELOPE = "paper_used_envelope"
+PAPER_USED_MONARCH = "paper_used_monarch"
+PAPER_USED_DL = "paper_used_dl"
+PAPER_USED_C5 = "paper_used_c5"
+PAPER_USED_OTHERS = "paper_used_others"
