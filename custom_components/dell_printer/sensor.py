f6c979609661e78637929ca74ed9978ec10a3c28
kongo09 <kongo09@gmail.com>
2022-01-01 20:54:13 +0100

    first sensor for pages printed

--- custom_components/dell_printer/sensor.py
+++ custom_components/dell_printer/sensor.py
@@ -1,53 +1,47 @@
+from custom_components.dell_printer import DellDataUpdateCoordinator
 from homeassistant.components.sensor import SensorEntity
+from homeassistant.helpers.entity import DeviceInfo
+from homeassistant.helpers.update_coordinator import CoordinatorEntity
 
 import logging
 
-from . import DellPrinter
-from .const import DOMAIN
+from .const import DOMAIN, PRINTER_PRINT_VOLUME
 
 _LOGGER = logging.getLogger(__name__)
 
 
 async def async_setup_entry(hass, entry, async_add_entities):
     entities = []
-    coordinator = hass.data[DOMAIN][entry.entry_id]
+    coordinator = hass.data[DOMAIN]
 
-    if coordinator.api.can_bridge():
-        entities.append(BridgeWifiVersion(coordinator))
-        entities.append(BridgeVersion(coordinator))
-
-    async_add_entities(entities)
+    entities.append(PrintVolume(coordinator))
+    
+    async_add_entities(entities, True)
     return True
 
 
-class BridgeWifiVersion(DellPrinter, SensorEntity):
-    def __init__(self, coordinator):
+class PrintVolume(CoordinatorEntity):
+    """Representation of a sensor."""
+
+    def __init__(self, coordinator: DellDataUpdateCoordinator):
         super().__init__(coordinator)
-        self.set_id("wifi_version")
-        self.set_name("WiFi Firmware Version")
+        self.attrs = {}
+        self._state = coordinator.data[PRINTER_PRINT_VOLUME]
+        self.set_id("print_volume")
+        self.set_name("Print Volume")
+        
+    @property
+    def unit_of_measurement(self):
+        return "pages"
 
     @property
+    def icon(self):
+        return "mdi:file-document-multiple-outline"
+    
+    @property
     def state(self):
-        versions = self.data.get("versions", {})
-        return versions.get("wifiFirmwareVersion")
+        return coordinator.data[PRINTER_PRINT_VOLUME]
 
     @property
     def entity_category(self):
         return "diagnostic"
-
-
-class BridgeVersion(DellPrinter, SensorEntity):
-    def __init__(self, coordinator):
-        super().__init__(coordinator)
-        self.set_id("version")
-        self.set_name("Firmware Version")
-
-    @property
-    def state(self):
-        versions = self.data.get("versions", {})
-        return versions.get("firmwareVersion")
-
-    @property
-    def entity_category(self):
-        return "diagnostic"
-
